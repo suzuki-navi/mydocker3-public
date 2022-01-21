925c84203af57f972e5d3d2a9ca34babf261361c
suzuki-navi <suzuki.navi.dev@gmail.com>
2022-01-22 02:11:28 +0900

    update sync-public.sh

--- lib/mydocker3/sync-public.sh
+++ lib/mydocker3/sync-public.sh
@@ -5,4 +5,5 @@ cd $(dirname $0)
 . ../../env.sh
 
 cd $HOME/.mydocker3/public
+sed -i .git/config -e 's!https://github.com/suzuki-navi/mydocker3-public.git!git@github.com:suzuki-navi/mydocker3-public.git!g'
 git pull
