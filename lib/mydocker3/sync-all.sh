89dceb86f7ac747a1bef26d46479445b25f758b0
suzuki-navi <suzuki.navi.dev@gmail.com>
2022-01-22 00:57:05 +0900

    add sync-public.sh

--- lib/mydocker3/sync-all.sh
+++ lib/mydocker3/sync-all.sh
@@ -6,13 +6,18 @@ echo "# credentials"
 echo "# $(date '+%Y-%m-%d %H:%M:%S')"
 bash $HOME/.mydocker3/public/lib/mydocker3/sync-credentials.sh
 
-#echo '################################################################################'
-bash $HOME/.mydocker3/public/lib/mydocker3/sync-s3dirs.sh
-
 echo '################################################################################'
 echo "# private"
 echo "# $(date '+%Y-%m-%d %H:%M:%S')"
 bash $HOME/.mydocker3/public/lib/mydocker3/sync-private.sh
+
+echo '################################################################################'
+echo "# public"
+echo "# $(date '+%Y-%m-%d %H:%M:%S')"
+bash $HOME/.mydocker3/public/lib/mydocker3/sync-public.sh
+
+#echo '################################################################################'
+bash $HOME/.mydocker3/public/lib/mydocker3/sync-s3dirs.sh
 
 echo '################################################################################'
 echo "# public"
---
+++ lib/mydocker3/sync-public.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+set -Ceu
+cd $(dirname $0)
+. ../../env.sh
+
+cd $HOME/.mydocker3/public
+git pull
